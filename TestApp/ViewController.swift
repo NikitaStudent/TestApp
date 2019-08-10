3dd8e70033fa9d7f826d82bc2d48d83d94315e7b
NikitaStudent <NKA_2000_00@mail.ru>
2019-08-10 23:10:53 +0300

    в этой части я дописал код который меняет название lable при вводе текста в TextBox  и нажатии кнопки done

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+       NameLabel.text = textField.text
+    }
+    
+    // MARK: Properties
+    @IBOutlet weak var nameTextView: UITextField!
+    @IBOutlet weak var NameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Handle the text field's user input trough delegate callbacks
+        nameTextView.delegate = self
     }
 
-
+    // MARK: actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        NameLabel.text = "Заголовок"
+    }
+    
 }
 
